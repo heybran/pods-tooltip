cad6d8a5ac9c241c4e36d56c2c9b76961a926f17
brandon <gwonzhang@gmail.com>
2023-07-26 15:12:26 +0800

    Improve tooltip logic and add a defer method

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ export default class PodsTooltip extends HTMLElement {
     super();
     this.attachShadow({ mode: 'open' });
     document.addEventListener('click', this.closeTooltipWhenClickOutside.bind(this));
+    this.defer = (window.requestIdleCallback || requestAnimationFrame).bind(window);
   }
 
   connectedCallback() {
@@ -14,17 +15,27 @@ export default class PodsTooltip extends HTMLElement {
         <button 
           part="button" 
           class="tooltip-button" 
-          onclick="this.getRootNode().host.closeTooltipWhenOpen()"
+          onclick="this.getRootNode().host.toggleTooltip()"
           onfocusin="this.getRootNode().host.openTooltip()"
           >
           <svg xmlns="http://www.w3.org/2000/svg" role="presentation" aria-hidden="true" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-help-circle"><circle cx="12" cy="12" r="10"></circle><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
           <span class="sr-only"></span>
         </button>
         <div slot="content" class="tooltip-content">
-          <slot></slot>
+          ${this.innerHTML}
         </div>
       </div>
     `;
+    this.tooltipContent.addEventListener('transitionend', () => {
+      if (this.tooltipContent.classList.contains('visible')) {
+        this.ariaExpanded = 'true';
+      } else {
+        this.ariaExpanded = 'false';
+      }
+    });
+    this.defer(() => {
+      this.innerHTML = `<!-- Original content has been moved into shadowRoot -->`;
+    });
   }
 
   get button() {
@@ -37,21 +48,20 @@ export default class PodsTooltip extends HTMLElement {
 
   closeTooltip() {
     this.tooltipContent.classList.remove('visible');
-    this.ariaExpanded = 'false';
-    this.button.removeEventListener('click', this.closeTooltipWhenOpen.bind(this));
+    // this.button.removeEventListener('click', this.closeTooltipWhenOpen.bind(this));
   }
 
   openTooltip() {
     this.tooltipContent.classList.add('visible');
-    this.tooltipContent?.addEventListener('transitionend', () => {
-      if (!this.tooltipContent?.classList.contains('visible')) return;
-      this.ariaExpanded = 'true';
-    });
   }
 
-  closeTooltipWhenOpen(e) {
-    if (this.getAttribute('aria-expanded') === 'false') return;
-    this.closeTooltip();
+  toggleTooltip(e) {
+    console.log(1);
+    if (this.getAttribute('aria-expanded') === 'false') {
+      this.openTooltip();
+    } else {
+      this.closeTooltip();
+    }
   }
 
   closeTooltipWhenClickOutside(e) {
@@ -64,10 +74,6 @@ export default class PodsTooltip extends HTMLElement {
       :host {
         display: inline-flex;
         color: #fff;
-      }
-
-      ::slotted(a) {
-        color: inherit;
       }
 
       button {
@@ -105,6 +111,13 @@ export default class PodsTooltip extends HTMLElement {
         bottom: calc(100% + 12px);
         width: max(25em, 350px);
         z-index: 9999;
+        display: grid;
+        gap: .75em;
+      }
+
+      .tooltip-content * {
+        color: inherit;
+        margin: 0;
       }
 
       .tooltip-content::before {
@@ -120,11 +133,6 @@ export default class PodsTooltip extends HTMLElement {
         background: var(--tooltip-background-color, #333);
         transform: rotate(45deg);
         left: calc(50% - var(--tooltip-arrow-size) / 2);
-      }
-
-      .tooltip-button:focus-visible + .tooltip-content {
-        opacity: 1;
-        pointer-events: initial;
       }
 
       .tooltip-content.visible {
